c42c4c87d4cb6605352e9c3c717ce9d05d96fe3a
Francois Pazery <asper-01@hotmail.fr>
2024-05-15 17:20:01 +0200

    modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-import { defineConfig } from 'astro/config'
+// astro.config.mjs
+
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
   site: 'https://asper-01.github.io',
   base: '',
-})
+  pages: [
+    {
+      path: '/work', // Chemin de la section de travail
+      component: './src/pages/work/index.astro', // Chemin vers le fichier index.astro de la section de travail
+    },
+    // Ajoutez d'autres entrées de page au besoin
+  ],
+});
